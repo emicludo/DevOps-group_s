68ccf104547af9c4768d2b8581c81610ba2e6662
emicludo <emg@cludo.com>
2023-03-24 11:24:23 +0100

    add the counter to error handler

--- itu-minitwit-node/app.js
+++ itu-minitwit-node/app.js
@@ -74,6 +74,10 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  httpRequestCounter.inc({ method: req.method, status: err.status || 500 });
+  // render the error page
+  res.status(err.status || 500).send(err.message);
 });
 
 module.exports = app;
