ec54c201379b28004d0085bdc77d79a91d86e182
emicludo <emg@cludo.com>
2023-03-24 11:23:08 +0100

    add httpRequestCounter

--- itu-minitwit-node/app.js
+++ itu-minitwit-node/app.js
@@ -3,14 +3,21 @@ var express = require('express');
 var session = require('express-session');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var rfs = require('rotating-file-stream');
 
 //Prometheus metrics
 const client = require('prom-client');
 const collectDefaultMetrics = client.collectDefaultMetrics;
 
-//Routing
+//Create a new Prometheus counter to track the number of HTTP requests
+const httpRequestCounter = new client.Counter({
+  name: 'http_requests_total',
+  help: 'Total number of HTTP requests',
+  labelNames: ['method', 'status']
+});
+collectDefaultMetrics();
+
+
+//Routing imports
 var indexRouter = require('./src/routes/index');
 var messageRouter = require('./src/routes/message');
 var followRouter = require('./src/routes/follow');
@@ -18,20 +25,11 @@ var unfollowRouter = require('./src/routes/unfollow');
 var signupRouter = require('./src/routes/signup');
 var signinRouter = require('./src/routes/signin');
 var signoutRouter = require('./src/routes/signout');
-/* var userRouter = require('./src/routes/user'); */
+
 //Simulator routing
 var simulatorRouter = require('./src/routes/simulator');
 
 var app = express();
-
-/* // create a rotating write stream
-var accessLogStream = rfs.createStream('access.log', {
-  interval: '1d',
-  path: path.join(__dirname, 'log')
-}) */
-
-/* // setting up the logger
-app.use(logger('dev', {stream: accessLogStream})) */
 
 app.use(session({
   secret: 'c2b71086dd6ba3b83431e00118d52c0fd2f178f439910fe7bf7e86a2a163e26f83932fac1f908015d7815bf0a817914e38ee56d904888337bff57c91c76ae8b1',
@@ -42,11 +40,13 @@ app.use(session({
 // view engine setup
 app.set('views', path.join(__dirname, '/src/views'));
 app.set('view engine', 'jade');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Routing
 app.use('/api/message', messageRouter);
 app.use('/api/follow', followRouter);
 app.use('/api/unfollow', unfollowRouter);
@@ -54,7 +54,14 @@ app.use('/api/signup', signupRouter);
 app.use('/api/signin', signinRouter);
 app.use('/api/signout', signoutRouter);
 app.use('/api/', indexRouter);
-/* app.use('/api/users', userRouter); */
+
+//Metrics endpoint - consumed by Prometheus
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.send(await client.register.metrics());
+});
+
+//Simulator routing
 app.use('/', simulatorRouter);
 
 // catch 404 and forward to error handler
@@ -62,16 +69,11 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-  
-  // render the error page
-  //res.status(err.status || 500);
-  //res.render('error');
 });
 
 module.exports = app;
