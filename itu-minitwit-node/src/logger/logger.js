f8b6604e4690a9bf9a1b5418ad73dec799b294d4
emicludo <emg@cludo.com>
2023-03-28 16:45:47 +0200

    update logger path

--- itu-minitwit-node/src/logger/logger.js
+++ itu-minitwit-node/src/logger/logger.js
@@ -13,7 +13,7 @@ const logger = createLogger({
     // - Write all logs with importance level of `error` or less to `error.log`
     // - Write all logs with importance level of `info` or less to `combined.log`
     //
-    new transports.File({ filename: './logs/error.log', level: 'error' }),
+    new transports.File({ filename: './../../logs/error.log', level: 'error' }),
     //new transports.File({ filename: './logs/combined.log' }),
   ],
 });
