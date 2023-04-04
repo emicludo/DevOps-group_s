9380f16f94a9716643c89cf547aee22e644a8cf1
Ingrid <inch@itu.dk>
2023-04-04 11:56:09 +0200

    remove comment

--- itu-minitwit-node/src/db/models/follower.model.js
+++ itu-minitwit-node/src/db/models/follower.model.js
@@ -4,8 +4,6 @@ const { DataTypes } = require('sequelize');
   // This function will automatically receive as parameter the Sequelize connection object.
   module.exports = (sequelize) => {
     sequelize.define('follower', {
-      // The following specification of the 'id' attribute could be omitted
-      // since it is the default.
       who_id: {
         type: DataTypes.INTEGER
       },
