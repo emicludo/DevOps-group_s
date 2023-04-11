6c49ef1b70b63718a48b5d722c7a661c1a1b3d7f
Ingrid <inch@itu.dk>
2023-04-11 17:42:41 +0200

    first test

---
+++ itu-minitwit-node/cypress/e2e/test.cy.js
@@ -0,0 +1,5 @@
+describe('Test test', () => {
+  it('should visit website', () => {
+    cy.visit('https://example.cypress.io')
+  })
+})
---
+++ itu-minitwit-node/cypress/e2e/timeline.cy.js
@@ -0,0 +1,5 @@
+describe('Testing the public timeline', () => {
+    it('successfully loads', () => {
+        cy.visit('/')
+    })
+})
