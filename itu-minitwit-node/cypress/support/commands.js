e57a37adc81f32de85bdec31598373a605abdf58
Ingrid <inch@itu.dk>
2023-04-13 14:45:13 +0200

    added login command

--- itu-minitwit-node/cypress/support/commands.js
+++ itu-minitwit-node/cypress/support/commands.js
@@ -9,17 +9,13 @@
 // ***********************************************
 //
 //
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('login', (username, password) => {
+    cy.session([username, password], () => {
+      cy.visit('/signin')
+      cy.get('#username-form').type(username)
+      cy.get('#password-form').type(password)
+      cy.get('#submit-btn').click()
+      cy.contains('h2', 'My Timeline')
+    })
+  })
