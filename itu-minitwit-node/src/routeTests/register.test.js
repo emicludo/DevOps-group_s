4bbc3dd7149e95e9e3d5e10bbb9aa6322d6236b0
matej <matej.basta@email.cz>
2023-04-18 18:11:14 +0200

    changing the tests in the register.test.js

--- itu-minitwit-node/src/routeTests/register.test.js
+++ itu-minitwit-node/src/routeTests/register.test.js
@@ -1,8 +1,9 @@
-const request = require('supertest');
+/* const request = require('supertest');
 const app = require('../../app');
 
+
 describe('POST /register', () => {
-  it('returns 403 if authorization header is not correct', async () => {
+  test('returns 403 if authorization header is not correct', async () => {
     const response = await request(app)
       .post('/register')
       .set('Authorization', 'incorrect_token')
@@ -13,5 +14,32 @@ describe('POST /register', () => {
       });
 
     expect(response.status).toBe(403);
+  });
+}); */
+
+const request = require('supertest');
+const app = require('../../app');
+const getAllUsers = require('../model/user');
+
+jest.mock('../model/user');
+
+describe('POST /register', () => {
+
+  test('returns 204 all fine', async () => {
+
+    getAllUsers.mockResolvedValue([]);
+
+    const response = await request(app)
+      .post('/register')
+      .set('Authorization', 'Basic c2ltdWxhdG9yOnN1cGVyX3NhZmUh')
+      .send({
+        username: 'testuser',
+        email: 'testuser@example.com',
+        pwd: null
+      });
+
+    expect(response.status).toBe(400);
+    expect(response.body).toEqual({});
+    expect(getAllUsers).toHaveBeenCalled();
   });
 });
