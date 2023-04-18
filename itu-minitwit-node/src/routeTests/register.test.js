2bd92cede5e12826b9de4ae8c6760ad10c245950
matej <matej.basta@email.cz>
2023-04-18 19:34:37 +0200

    register: testing when using a username already in use

--- itu-minitwit-node/src/routeTests/register.test.js
+++ itu-minitwit-node/src/routeTests/register.test.js
@@ -1,8 +1,11 @@
-/* const request = require('supertest');
+const request = require('supertest');
 const app = require('../../app');
+const getAllUsers = require('../model/user');
 
+jest.mock('../model/user');
 
 describe('POST /register', () => {
+
   test('returns 403 if authorization header is not correct', async () => {
     const response = await request(app)
       .post('/register')
@@ -15,19 +18,10 @@ describe('POST /register', () => {
 
     expect(response.status).toBe(403);
   });
-}); */
 
-const request = require('supertest');
-const app = require('../../app');
-const getAllUsers = require('../model/user');
+  test.only('returns 400 if the username is already taken', async () => {
 
-jest.mock('../model/user');
-
-describe('POST /register', () => {
-
-  test('returns 204 all fine', async () => {
-
-    getAllUsers.mockResolvedValue([]);
+    getAllUsers.mockResolvedValue(['foo', 'testuser']);
 
     const response = await request(app)
       .post('/register')
@@ -39,7 +33,8 @@ describe('POST /register', () => {
       });
 
     expect(response.status).toBe(400);
-    expect(response.body).toEqual({});
+    expect(response.body).toHaveProperty('status');
+    expect(response.body).toHaveProperty('error_msg');
     expect(getAllUsers).toHaveBeenCalled();
   });
 });
