5a59d0e4772eaf2b5ab74b2b924b3ac0ae2d4a6a
matej <matej.basta@email.cz>
2023-04-18 19:45:34 +0200

    register: test response when username is taken, when password is missing, when email is not in correct format

--- itu-minitwit-node/src/routeTests/register.test.js
+++ itu-minitwit-node/src/routeTests/register.test.js
@@ -19,9 +19,28 @@ describe('POST /register', () => {
     expect(response.status).toBe(403);
   });
 
-  test.only('returns 400 if the username is already taken', async () => {
+  test('returns 400 if the username is already taken', async () => {
 
-    getAllUsers.mockResolvedValue(['foo', 'testuser']);
+    getAllUsers.mockResolvedValue([{username: 'foo'}, {username: 'testuser'}]);
+
+    const response = await request(app)
+      .post('/register')
+      .set('Authorization', 'Basic c2ltdWxhdG9yOnN1cGVyX3NhZmUh')
+      .send({
+        username: 'testuser',
+        email: 'testuser@example.com',
+        pwd: 'testpassword'
+      });
+
+    expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty('status');
+    expect(response.body).toHaveProperty('error_msg');
+    expect(getAllUsers).toHaveBeenCalled();
+  });
+
+  test('returns 400 if the password is missing', async () => {
+
+    getAllUsers.mockResolvedValue([]);
 
     const response = await request(app)
       .post('/register')
@@ -30,6 +49,25 @@ describe('POST /register', () => {
         username: 'testuser',
         email: 'testuser@example.com',
         pwd: null
+      });
+
+    expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty('status');
+    expect(response.body).toHaveProperty('error_msg');
+    expect(getAllUsers).toHaveBeenCalled();
+  });
+
+  test('returns 400 if the email is not in correct format', async () => {
+
+    getAllUsers.mockResolvedValue([]);
+
+    const response = await request(app)
+      .post('/register')
+      .set('Authorization', 'Basic c2ltdWxhdG9yOnN1cGVyX3NhZmUh')
+      .send({
+        username: 'testuser',
+        email: 'testuserexample.com',
+        pwd: 'testpassword'
       });
 
     expect(response.status).toBe(400);
