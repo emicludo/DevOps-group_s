f579e67a91fb252bf3e88f8a0a066e844bc13cf1
matej <matej.basta@email.cz>
2023-04-19 21:52:11 +0200

    mocking the database and getAllUsers function

--- itu-minitwit-node/src/routeTests/msgs.test.js
+++ itu-minitwit-node/src/routeTests/msgs.test.js
@@ -2,3 +2,6 @@ const request = require('supertest');
 const app = require('../../app');
 const getAllUsers = require('../model/user');
 const database = require('../db/dbService');
+
+jest.mock('../model/user');
+jest.mock('../db/dbService');
