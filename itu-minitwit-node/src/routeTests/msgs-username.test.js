e485320c70843fbcb1d9d7a60503ff4b934ca63a
matej <matej.basta@email.cz>
2023-04-20 17:18:22 +0200

    msgs-username: 403 if header is not from simulator

---
+++ itu-minitwit-node/src/routeTests/msgs-username.test.js
@@ -0,0 +1,38 @@
+const request = require('supertest');
+const app = require('../../app');
+const database = require('../db/dbService');
+
+jest.mock('../db/dbService');
+
+describe('GET /msgs/:username', () => {
+
+    test('returns 403 if authorization header is not correct', async () => {
+        const response = await request(app)
+          .get('/msgs/testuser')
+          .set('Authorization', 'incorrect_token');
+    
+        expect(response.status).toBe(403);
+        expect(response.body.error_msg).toBe("You are not authorized to use this resource!");
+    });
+
+    /*test('returns 500 if the database does not work properly', async () => {  
+      database.all = jest.fn((sql, params, callback) => {
+        callback('Error', null);
+      });
+      const response = await request(app)
+        .get('/msgs')
+        .set('Authorization', 'Basic c2ltdWxhdG9yOnN1cGVyX3NhZmUh')
+      expect(response.status).toBe(500);
+    });
+
+    test('returns messages if everything is fine', async () => {  
+      database.all = jest.fn((sql, params, callback) => {
+        callback(null, [{text: 'hello world', pubDate: '20-4-2020', username: 'testuser'}]);
+      });
+      const response = await request(app)
+        .get('/msgs')
+        .set('Authorization', 'Basic c2ltdWxhdG9yOnN1cGVyX3NhZmUh')
+        expect(response.body).toEqual([{content: 'hello world', pubDate: '20-4-2020', user: 'testuser'}]);
+    }); */
+
+});
