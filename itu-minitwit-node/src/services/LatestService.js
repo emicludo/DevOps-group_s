bf4405bfca3b68d0785ca1c4d321465dc0b563bc
emicludo <emg@cludo.com>
2023-04-25 16:42:13 +0200

    add database

--- itu-minitwit-node/src/services/LatestService.js
+++ itu-minitwit-node/src/services/LatestService.js
@@ -1,13 +1,37 @@
+const database = require('../db/dbService')
+
 module.exports = class LatestService {
-    constructor() {
-        this.latest = 0;
+    constructor() { }
+
+    async getLatest() {
+        try {
+            database.all(query, [no_msgs], (err, rows) => {
+                if (err) {
+                    logger.log('error', { url: req.url, method: req.method, requestBody: req.body, responseStatus: 500, message: err });
+                    var error = new Error("Error retrieving messages from our database");
+                    error.status = 500;
+                    next(error);
+                    return;
+                }
+                const filteredMsgs = [];
+                for (const msg of rows) {
+                    const filteredMsg = {};
+                    filteredMsg.content = msg.text; req.method
+                    filteredMsg.pubDate = msg.pubDate;
+                    filteredMsg.user = msg.username;
+                    filteredMsgs.push(filteredMsg);
+                }
+                res.send(filteredMsgs);
+            });
+        } catch (error) {
+            logger.log('error', { url: req.url, method: req.method, requestBody: req.body, responseStatus: 500, message: error });
+            var newError = new Error(error);
+            newError.status = 500;
+            next(newError);
+        }
     }
 
-    getLatest() {
-        return this.latest;
-    }
-
-    updateLatest(number) {
-      this.latest = number;
+    async updateLatest(number) {
+        this.latest = number;
     }
 }
