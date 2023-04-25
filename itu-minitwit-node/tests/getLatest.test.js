5feb4a0c33b342df46a95574e701e6ae90bd0d65
Matej-Basta <matej.basta@email.cz>
2023-04-25 20:53:16 +0200

    getLatest in Mocha

---
+++ itu-minitwit-node/tests/getLatest.test.js
@@ -0,0 +1,41 @@
+const request = require('supertest');
+const app = require('../app');
+const LatestService = require('../src/services/LatestService');
+const sinon = require('sinon');
+const { expect } = require('chai');
+
+describe('GET /latest', () => {
+
+  // successful response
+  it('responds with 200 and the latest value', async () => {
+    // Mock the latestService.getLatest function
+    const getLatestStub = sinon.stub(LatestService.prototype, 'getLatest').returns(42);
+
+    // Make a GET request to the endpoint
+    const response = await request(app).get('/latest');
+
+    // Check that the response has a 200 status code
+    expect(response.status).to.equal(200);
+
+    // Check that the response body contains the latest value
+    expect(response.body).to.deep.equal({ latest: 42 });
+
+    // Restore the stub
+    getLatestStub.restore();
+  });
+
+  // error response
+  it('responds with 500 if there is an error', async () => {
+    // Mock the latestService.getLatest function to throw an error
+    const getLatestStub = sinon.stub(LatestService.prototype, 'getLatest').throws(new Error('Test error'));
+
+    // Make a GET request to the endpoint
+    const response = await request(app).get('/latest');
+
+    // Check that the response has a 500 status code
+    expect(response.status).to.equal(500);
+
+    // Restore the stub
+    getLatestStub.restore();
+  });
+});
