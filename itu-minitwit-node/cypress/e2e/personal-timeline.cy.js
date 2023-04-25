c7fb2e746a2254df9de34a64f75340c4366dc35e
Ingrid <inch@itu.dk>
2023-04-25 10:30:01 +0200

    personal timeline tests

--- itu-minitwit-node/cypress/e2e/personal-timeline.cy.js
+++ itu-minitwit-node/cypress/e2e/personal-timeline.cy.js
@@ -1,19 +1,28 @@
+import '../support/commands'
+
 describe('Testing the user timeline', () => {
-    before(() => {
-        cy.visit('/')
+    beforeEach(() => {
         cy.login('test-user', 'password123')
     })
 
-    it('title is "Public Timeline"', () => {
+    it('title is "My Timeline"', () => {
+        cy.visit('/')
         cy.contains('h2', 'My Timeline')
+        cy.get('#twit-form').type('Super relevant message')
     })
 
     it('writing a tweet', () => {
         cy.intercept('POST', '/api/message').as('new-msg')
+        cy.visit('/')
+        cy.get('#twit-form').type('Super relevant message').should('have-value', 'Super relevant message')
+        cy.get('#twit-submit-btn').click()
+    })
 
+    it('writing a tweet, displays the tweet', () => {
+        cy.intercept('POST', '/api/message').as('new-msg')
+        cy.visit('/')
         cy.get('#twit-form').type('Super relevant message')
         cy.get('#twit-submit-btn').click()
 
-        
     })
 })
