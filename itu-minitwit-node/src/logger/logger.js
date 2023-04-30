f259d86d9a9c339a9e3b90340229220374e173bb
emicludo <emg@cludo.com>
2023-04-30 15:20:02 +0200

    remove console logger

--- itu-minitwit-node/src/logger/logger.js
+++ itu-minitwit-node/src/logger/logger.js
@@ -15,7 +15,7 @@ const logger = winston.createLogger({
   transports: [
     new winston.transports.File({ filename: './logs/errors.log', level: 'error' }),
     new winston.transports.File({ filename: './logs/combined.log', level: 'info'  }),
-    new winston.transports.Console(),
+    //new winston.transports.Console(),
     esTransport,
   ],
 });
