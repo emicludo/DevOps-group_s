1691c6c92ecf19ce4a3164bec1a6f189ca371ddf
Matej-Basta <matej.basta@email.cz>
2023-04-30 21:44:20 +0200

    finishing post for msgs-username tests

--- itu-minitwit-node/tests/msgs-username.test.js
+++ itu-minitwit-node/tests/msgs-username.test.js
@@ -80,3 +80,71 @@ describe('GET /msgs/:username', () => {
       });
 
 })
+
+describe('POST /msgs/:username', () => {
+
+    afterEach(() => {
+        sandbox.restore();
+      });
+    
+    it('returns 403 if authorization header is not correct', async () => {
+      const response = await request(app)
+        .post('/msgs/testuser')
+        .set('Authorization', 'incorrect_token')
+        .send({
+            content: 'testcontent'
+          });
+
+      expect(response.status).to.equal(403);
+      expect(response.body.error_msg).to.equal("You are not authorized to use this resource!");
+    });
+
+    it('returns 404 if the user is not in the database', async () => {
+        const getAllUsersStub = sandbox.stub(getAllUsers.prototype, 'getAllUsers').resolves([{ username: 'foo' }]);
+        
+        const response = await request(app)
+          .post('/msgs/testuser')
+          .set('Authorization', 'Basic c2ltdWxhdG9yOnN1cGVyX3NhZmUh')
+          .send({
+              content: 'testcontent'
+            });
+  
+        expect(response.status).to.equal(404);
+        expect(response.body.error_msg).to.equal("User is not on our database");
+      });
+
+      it('returns 500 if the database does not work properly', async () => {
+        const getAllUsersStub = sandbox.stub(getAllUsers.prototype, 'getAllUsers').resolves([{ username: 'testuser' }]);
+    
+        sandbox.stub(database, 'add').callsFake((table, data, callback) => {
+            callback('Error', null);
+          });
+    
+          const response = await request(app)
+          .post('/msgs/testuser')
+          .set('Authorization', 'Basic c2ltdWxhdG9yOnN1cGVyX3NhZmUh')
+          .send({
+              content: 'testcontent'
+            });
+    
+        expect(response.status).to.equal(500);
+      });
+
+      it('returns 204 if everything is fine', async () => {
+        const getAllUsersStub = sandbox.stub(getAllUsers.prototype, 'getAllUsers').resolves([{ username: 'testuser' }]);
+    
+        sandbox.stub(database, 'add').callsFake((table, data, callback) => {
+            callback(null, 'Success');
+          });
+    
+          const response = await request(app)
+          .post('/msgs/testuser')
+          .set('Authorization', 'Basic c2ltdWxhdG9yOnN1cGVyX3NhZmUh')
+          .send({
+              content: 'testcontent'
+            });
+    
+        expect(response.status).to.equal(204);
+        expect(response.body).to.deep.equal({});
+      });
+});
