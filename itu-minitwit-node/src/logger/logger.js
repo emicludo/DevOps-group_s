ae651213e776b9851eeb4ca9400cb4bffb8e4537
emicludo <emg@cludo.com>
2023-05-03 09:06:34 +0200

    code cleanup

--- itu-minitwit-node/src/logger/logger.js
+++ itu-minitwit-node/src/logger/logger.js
@@ -1,13 +1,5 @@
 const winston = require('winston')
-const { ElasticsearchTransport } = require('winston-elasticsearch');
 const ecsFormat = require('@elastic/ecs-winston-format')
-
-const esTransport = new ElasticsearchTransport({
-  level: 'info',
-  index: 'minitwit-logs',
-  clientOpts: { node: 'http://104.248.16.95:9200' },
-});
-
 
 const logger = winston.createLogger({
   level: 'info',
@@ -15,15 +7,13 @@ const logger = winston.createLogger({
   transports: [
     new winston.transports.File({ filename: '/app/logs/errors.log', level: 'error' }),
     new winston.transports.File({ filename: '/app/logs/combined.log', level: 'info'  }),
-    //new winston.transports.Console(),
-    //esTransport,
   ],
 });
 
-//
+
 // If we're not in production then log to the `console` with the format:
 // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-//
+
 /* if (process.env.NODE_ENV !== 'production') {
   logger.add(
     new winston.transports.Console({
